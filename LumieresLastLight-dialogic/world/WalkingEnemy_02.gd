887e3e47c289d4a37b60a01a166eb7777ff23d58
ninapsis <anacarolinacardoso@live.com>
2024-03-18 23:09:47 -0300

    parallax

---
+++ LumieresLastLight-dialogic/world/WalkingEnemy_02.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+const EnemyDeathEffectScene = preload("res://effects/enemy_death_effect.tscn")
+
+@export var speed = 30.0
+@export var turns_at_ledges = true
+
+var gravity = 200.0
+var direction = 1.0
+
+@onready var floor_cast = $FloorCast
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if is_on_wall():
+		turn_around()
+	
+	if is_at_ledge() and turns_at_ledges:
+		turn_around()
+	
+	velocity.x = direction * speed 
+	animated_sprite_2d.scale.x = direction 	
+	
+	move_and_slide()
+
+func is_at_ledge():
+	return is_on_floor() and not floor_cast.is_colliding()
+
+func turn_around():
+	direction *= -1.0
+
+func _on_hurtbox_hurt(_hitbox, damage):
+	stats.health -= damage
+
+func _on_stats_no_health():
+	Utils.instantiate_scene_on_world(EnemyDeathEffectScene, death_effect_location.global_position)
+	queue_free()
